985beeb35393623eb6873ae953dc7803a71c5abc
Simon Ledoux <simon@simon511000.fr>
2024-04-08 09:43:27 +0000

    feat: promote the new extension

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-import { recupererToutesLesNotesTriees, ajouterBoutonSauvegarde, recreerTableau } from "./helpers";
-import * as browser from 'webextension-polyfill';
+const alertDiv = document.createElement('div');
+alertDiv.classList.add('alert', 'alert-danger', 'foot-weight-bold', 'display-6');
+alertDiv.innerHTML = `Important: L'extension MiaouNotes n'est plus maintenue et est donc obsolète.<br>Téléchargez son successeur, <a href="https://chromewebstore.google.com/detail/better-iut-rcc/jofahdhjofjoackgkaodimfhnbfkgnbj?hl=fr" target="_blank">Better IUT RCC</a> (aussi disponible sur Firefox et Android) dès maintenant!`;
+
+document.querySelector('#mainContent').prepend(alertDiv);
 
 // Récupération des notes connues
-browser.storage.sync.get('notesAlreadyKnow').then((result) => {
+/*browser.storage.sync.get('notesAlreadyKnow').then((result) => {
     const notesConnues: number[] = result.notesAlreadyKnow || []
 
     const tableau: HTMLTableElement = document.querySelector("#mainContent > div.row > div:nth-child(5) > div > div > table")
@@ -29,4 +32,4 @@ browser.storage.sync.get('notesAlreadyKnow').then((result) => {
             location.reload()
         })
     })
-})
+})*/
